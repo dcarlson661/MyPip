deeb7463ed9c812f02192b6f82517259fe76aeb6
dcarl661 <dcarl@earthlink.net>
2018-03-26 11:19:35 -0700

    add pip set up in manifest and basic in activityMain

--- app/src/main/java/com/dcarl661/mypip/MainActivity.java
+++ app/src/main/java/com/dcarl661/mypip/MainActivity.java
@@ -1,13 +1,43 @@
 package com.dcarl661.mypip;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void GoPIP(View v){
+            enterPictureInPictureMode();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onPictureInPictureModeChanged(boolean isInPictureInPictureMode, Configuration newConfig) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig);
+
+        TextView textView=findViewById(R.id.textView);
+        Button button=findViewById(R.id.button);
+
+        if(isInPictureInPictureMode){
+            //going into
+            button.setVisibility(View.INVISIBLE);
+            getSupportActionBar().hide();
+            textView.setText("Harry");
+        }
+        else{
+            //going out
+            getSupportActionBar().show();
+            button.setVisibility(View.VISIBLE);
+            textView.setText("Sally");
+        }
+    }
+
 }
